f55d466cd5c0f1dfd7b349a3d43ab9a010fdaaa5
Ömer Faruk ARAN <omerfaruk.aran@hotmail.com>
2026-01-05 01:31:48 +0300

    Feature: Added log deduplication helpers and new configuration for message change logging

--- components/samsung_ac/log.h
+++ components/samsung_ac/log.h
@@ -1,7 +1,10 @@
+// log.h
 #pragma once
 
 #include "esphome/core/log.h"
 #include "util.h"
+#include <string>
+#include <cstdint>
 
 #define LOGV(...) ESP_LOGV(TAG, __VA_ARGS__)
 #define LOGD(...) ESP_LOGD(TAG, __VA_ARGS__)
@@ -21,9 +24,21 @@ namespace esphome
 {
     namespace samsung_ac
     {
+
         extern bool debug_log_messages;
         extern bool debug_log_undefined_messages;
         extern bool debug_log_raw_bytes;
+
+        // NEW:
+        extern bool debug_log_messages_on_change;
+
+        // Dedup helpers
+        uint32_t log_dedup_key(const std::string &source, const std::string &dest, uint16_t id);
+        uint32_t fnv1a_32(const char *data, size_t len);
+        uint32_t fnv1a_32(const std::string &s);
+
+        // Returns true if we should print (first time / changed / optional interval)
+        bool log_should_print(uint32_t key, double value, double epsilon = 0.0, uint32_t min_interval_ms = 0);
 
     } // namespace samsung_ac
 } // namespace esphome
