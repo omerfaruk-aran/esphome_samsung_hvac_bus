5e372d07edc4350eed8bbf312ea9d0fc2de35246
Ömer Faruk ARAN <omerfaruk@aran.net.tr>
2026-01-04 13:14:39 +0300

    Add optional map_auto_to_heat_cool config flag

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -245,6 +245,7 @@ DEVICE_SCHEMA = cv.Schema(
         cv.Optional(CONF_DEVICE_MODE): SELECT_MODE_SCHEMA,
         cv.Optional(CONF_DEVICE_WATER_HEATER_MODE): SELECT_WATER_HEATER_MODE_SCHEMA,
         cv.Optional(CONF_DEVICE_CLIMATE): CLIMATE_SCHEMA,
+        cv.Optional(CONF_MAP_AUTO_TO_HEAT_COOL, default=False): cv.boolean,
         cv.Optional(CONF_DEVICE_CUSTOM, default=[]): cv.ensure_list(
             CUSTOM_SENSOR_SCHEMA
         ),
@@ -319,7 +320,7 @@ CONF_DEBUG_LOG_MESSAGES = "debug_log_messages"
 CONF_DEBUG_LOG_MESSAGES_RAW = "debug_log_messages_raw"
 
 CONF_NON_NASA_KEEPALIVE = "non_nasa_keepalive"
-
+CONF_MAP_AUTO_TO_HEAT_COOL = "map_auto_to_heat_cool"
 CONF_DEBUG_LOG_UNDEFINED_MESSAGES = "debug_log_undefined_messages"
 
 
@@ -526,6 +527,9 @@ async def to_code(config):
             await climate.register_climate(var_cli, conf)
             cg.add(var_dev.set_climate(var_cli))
 
+            # Optional UI mapping: expose Samsung Auto as HA Heat/Cool
+            cg.add(var_dev.set_map_auto_to_heat_cool(device.get(CONF_MAP_AUTO_TO_HEAT_COOL, False)))
+    
         if CONF_DEVICE_CUSTOM in device:
             for cust_sens in device[CONF_DEVICE_CUSTOM]:
                 sens = await sensor.new_sensor(cust_sens)
@@ -576,3 +580,4 @@ async def to_code(config):
 
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+
